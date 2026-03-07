c492de357e100ac96a89eca031d51e186c674980
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-07 21:03:16 +0900

    feat: 문자열 단어공부 #20

--- week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
+++ week2/problem-solving/난이도하_문자열_단어공부_브론즈1.py
@@ -1,2 +1,31 @@
 # 문자열 - 단어 공부 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/1157
+import sys
+input = sys.stdin.readline
+
+s = input().strip()
+s_list = []
+
+for char in s:
+  dic = {}
+  s_list.append(char.upper())
+
+str = ''.join(s_list)
+
+for i, value in enumerate(s_list[:]):
+  if not dic.get(value):
+    dic[value] = 1
+  else:
+    dic[value] += 1
+
+max_value = max(dic.values())
+max_key = None
+
+for key in dic:
+  if dic[key] == max_value:
+    if not max_key:
+      max_key = key
+    else:
+      max_key = '?'
+
+print(max_key)
