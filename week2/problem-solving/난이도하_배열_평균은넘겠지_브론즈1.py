fdc339d799e251df805e96f6731227bc0986d54a
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-07 20:03:15 +0900

    feat: 배열 평균은 넘겠지  #18

--- week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
+++ week2/problem-solving/난이도하_배열_평균은넘겠지_브론즈1.py
@@ -1,2 +1,15 @@
 # 배열 - 평균은 넘겠지 (백준 브론즈1)
 # 문제 링크: https://www.acmicpc.net/problem/4344
+
+n = int(input())
+
+for _ in range(n):
+  size_over_avg_students = 0
+  list_int = [int(x) for x in input().split()]
+  size_students = list_int.pop(0)
+  avg = sum(list_int) / size_students
+  size_over_avg_students = sum(i > avg for i in list_int)
+  
+  ratio_over_avg = size_over_avg_students / size_students * 100
+  print(f'{ratio_over_avg:.3f}%')
+
