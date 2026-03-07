95820a50ce544e9d7d63636f3b973a79ddaa7bb2
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-07 20:30:41 +0900

    feat: 문자열_문자열반복 #19

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,16 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for i in range(t):
+  p_list = []
+
+  r, s = input().strip().split(' ')
+
+  for i in s:
+    p_list.append(i*int(r))
+
+  print(''.join(p_list))
