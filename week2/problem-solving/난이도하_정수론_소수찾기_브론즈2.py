8631dda43005d33dd72604f6751bf5b34f4a9703
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-09 22:22:57 +0900

    feat: 소수 찾기 #22

--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,26 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+import sys
+input = sys.stdin.readline
+
+def is_prime(num):
+  if num < 2:
+    return False
+  if num == 2:
+    return True
+  if(num % 2 == 0):
+    return False
+  for i in range(3, int(num**0.5) + 1, 2):
+    if (num % i == 0):
+      return False
+  return True
+
+n = input()
+n_list = list(map(int, input().split()))
+prime_list = list()
+
+for i in n_list:
+  if(is_prime(int(i))):
+    prime_list.append(i)
+
+print(len(prime_list))
