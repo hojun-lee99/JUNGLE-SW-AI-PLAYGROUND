95d4229c49adb88a98c403e92b5894c2208832f3
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-13 22:31:03 +0900

    feat: implement stack class with push, pop, size, empty, and top methods #54

--- SW-AI-W02-05/week3/problem-solving/난이도하_스택_스택_실버4.py
+++ SW-AI-W02-05/week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,57 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+
+class MyStack:
+  def __init__(self):
+    self._stack = []
+    pass
+
+  def push(self, data):
+    self._stack.append(data)
+
+  def pop(self):
+    if(len(self._stack) == 0):
+      return -1
+    return self._stack.pop()
+  
+  def size(self):
+    return len(self._stack)
+  
+  def empty(self):
+    if(len(self._stack) == 0):
+      return 1
+    return 0
+  
+  def top(self):
+    if(len(self._stack) == 0):
+      return -1
+    return self._stack[len(self._stack) - 1]
+  
+def switchInput(input_list, stack):
+  if(input_list[0] == 'push'):
+    stack.push(input_list[1])
+    return
+  if(input_list[0] == 'pop'):
+    print(stack.pop())
+    return
+  if(input_list[0] == 'size'):
+    print(stack.size())
+    return
+  if(input_list[0] == 'empty'):
+    print(stack.empty())
+    return
+  if(input_list[0] == 'top'):
+    print(stack.top())
+    return
+  else:
+    return
+
+
+num = int(input())
+
+my_stack = MyStack()
+for i in range(num):
+  input_list = input().split()
+  switchInput(input_list, my_stack)
+
+
