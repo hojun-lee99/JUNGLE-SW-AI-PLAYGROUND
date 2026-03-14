1c73bd49ed6018cf56e5cab3648c272a3d4174c7
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-14 10:38:25 +0900

    feat: implement binary search algorithm for number finding problem #52

--- SW-AI-W02-05/week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
+++ SW-AI-W02-05/week3/problem-solving/난이도하_이분탐색_수찾기_실버4.py
@@ -1,2 +1,36 @@
 # 이분탐색 - 수 찾기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/1920
+import sys
+input = sys.stdin.readline
+
+def binary_search(n, list):
+  left = 0
+  right = len(list) - 1
+
+  while(left < right):
+    mid = (left + right) // 2
+    if(n == list[mid]):
+      return True
+    if(n < list[mid]):
+      right = mid - 1
+    if(n > list[mid]):
+      left = mid + 1
+
+  if(n == list[left] and n == list[right]):
+    return True
+  
+  return False
+
+n = int(input().strip())
+
+a_list = list(map(int, input().strip().split()))
+m = int(input().strip())
+m_list = list(map(int, input().strip().split()))
+
+a_list.sort()
+
+for i in range(len(m_list)):  
+  if(binary_search(m_list[i], a_list)):
+    print(1)
+  else:
+    print(0)
