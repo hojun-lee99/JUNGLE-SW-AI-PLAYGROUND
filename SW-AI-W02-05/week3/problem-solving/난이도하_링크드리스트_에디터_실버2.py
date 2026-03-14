d00e363c24438ac1e6af8f9da58ef01717a97938
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-14 16:58:10 +0900

    feat: implement linked list editor functionality for string manipulation #56

--- SW-AI-W02-05/week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
+++ SW-AI-W02-05/week3/problem-solving/난이도하_링크드리스트_에디터_실버2.py
@@ -1,2 +1,40 @@
 # 링크드리스트 - 에디터 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1406
+import sys
+input = sys.stdin.readline    
+
+def switch_command(command: list, string_stack: list, saved_stack: list):
+  if(command[0] == 'L'):
+    if(len(string_stack) == 0):
+      return
+    saved_stack.append(string_stack.pop())
+    return
+  if(command[0] == 'D'):
+    if(len(saved_stack) == 0):
+      return
+    string_stack.append(saved_stack.pop())
+    return
+  if(command[0] == 'B'):
+    if(len(string_stack) == 0):
+      return
+    string_stack.pop()
+    return
+  if(command[0] == 'P'):
+    string_stack.append(command[1])
+    return
+
+string_stack = list(input().strip())
+saved_stack = []
+
+n = int(input().strip())
+commands = []
+for _ in range(n):
+  commands.append(input().strip().split())
+
+for command in commands:
+  switch_command(command, string_stack, saved_stack)
+
+while(saved_stack):
+  string_stack.append(saved_stack.pop())
+
+print(''.join(string_stack))
