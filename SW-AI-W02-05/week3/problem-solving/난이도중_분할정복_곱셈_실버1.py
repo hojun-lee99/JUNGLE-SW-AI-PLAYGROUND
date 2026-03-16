3d1f84a8bf183f4c0896604e189042b1e4054d1d
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-16 11:39:44 +0900

    feat: implement divide and conquer algorithm for multiplication problem #59

--- SW-AI-W02-05/week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
+++ SW-AI-W02-05/week3/problem-solving/난이도중_분할정복_곱셈_실버1.py
@@ -1,2 +1,24 @@
 # 분할정복 - 곱셈 (백준 실버1)
 # 문제 링크: https://www.acmicpc.net/problem/1629
+"""
+((A^(B-1) % C) * 2 * 10) % C
+"""
+import sys
+input = sys.stdin.readline
+
+def divide_and_conquer(a, b, c):
+  if(b == 0):
+    return 1 % c
+  
+  value = divide_and_conquer(a, b//2, c)
+  
+  if(b % 2  == 1):
+    return (value * value * a) % c
+    
+  return (value * value) % c
+
+
+
+nums = list(map(int, input().split()))
+
+print(divide_and_conquer(nums[0], nums[1], nums[2]))
