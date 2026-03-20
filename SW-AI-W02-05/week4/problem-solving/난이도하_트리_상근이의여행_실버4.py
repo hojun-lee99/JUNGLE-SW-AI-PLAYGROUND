3c1af381fa606f42616755fc485694fd8b700ba0
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-20 21:10:19 +0900

    feat: 난이도하 트리 상근이의 여행 #84 - https://en.wikipedia.org/wiki/Minimum_spanning_tree

--- SW-AI-W02-05/week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
+++ SW-AI-W02-05/week4/problem-solving/난이도하_트리_상근이의여행_실버4.py
@@ -1,2 +1,38 @@
 # 트리 - 상근이의 여행 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/9372
+import sys
+input = sys.stdin.readline
+min_num = 0
+
+def dfs(graph, start, visited=None):
+  global min_num
+
+  if not visited: visited = []
+  if len(visited) == len(graph): return
+
+
+  visited.append(start)
+
+  for node in graph[start]:
+    if node not in visited:
+      min_num += 1
+      dfs(graph, node, visited)
+
+t = int(input())
+
+for i in range(t):
+  n, m = input().split()
+  graph = {}
+
+  for i in range(1, int(n) + 1):
+    graph[i] = []
+
+  for i in range(int(m)):
+    u, v = list(map(int, input().split()))
+    graph[u].append(v)
+    graph[v].append(u)
+
+  dfs(graph, 1)
+  print(min_num)
+
+  min_num = 0
