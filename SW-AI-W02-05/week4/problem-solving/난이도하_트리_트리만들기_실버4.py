8cb6daba23277286f1165ee99a3fd5d523e659f7
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-21 17:29:38 +0900

    feat: 트리 트리만들기 #85

--- SW-AI-W02-05/week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ SW-AI-W02-05/week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,20 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+import sys
+input = sys.stdin.readline
+
+def main():
+  num_nodes, num_leaves = list(map(int, input().split()))
+
+  if num_leaves == 2:
+    for i in range(num_nodes - 1):
+      print(f"{i} {i+1}")
+    return
+
+  for i in range(1, num_leaves + 1):
+    print(f"{0} {i}")
+
+  for i in range(num_leaves, num_nodes - 1):
+    print(f"{i} {i+1}")
+
+main()
