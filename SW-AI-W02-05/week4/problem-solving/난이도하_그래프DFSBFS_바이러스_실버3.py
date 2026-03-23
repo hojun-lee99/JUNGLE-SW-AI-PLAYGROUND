610133eb5d668ff88e059431a00382480e96d7bd
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-23 21:30:16 +0900

    feat: dfs,bfs 바이러스 #87

--- SW-AI-W02-05/week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
+++ SW-AI-W02-05/week4/problem-solving/난이도하_그래프DFSBFS_바이러스_실버3.py
@@ -1,2 +1,32 @@
 # 그래프, DFS, BFS - 바이러스 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/2606
+import sys
+input = sys.stdin.readline
+
+num_computer = int(input())
+num = int(input())
+
+network = {}
+for i in range(1, num_computer+1):
+  network[i] = []
+
+for j in range(num):
+  n, m = list(map(int, input().split()))
+  network[n].append(m)
+  network[m].append(n)
+
+visited = []
+queue = []
+queue.append(1)
+visited.append(1)
+num_virus_computer = 0
+
+while(queue):
+  current = queue.pop(0)
+  for node in network[current]:
+    if node not in visited:
+      num_virus_computer += 1
+      queue.append(node)
+      visited.append(node)
+
+print(num_virus_computer)
