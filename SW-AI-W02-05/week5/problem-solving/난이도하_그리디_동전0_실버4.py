3c60b5de7f60f4b35c1249344fb17c393b44189b
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-27 21:35:58 +0900

    feat: greedy 동전 0 풀이 구현 #116

--- SW-AI-W02-05/week5/problem-solving/난이도하_그리디_동전0_실버4.py
+++ SW-AI-W02-05/week5/problem-solving/난이도하_그리디_동전0_실버4.py
@@ -1,2 +1,20 @@
 # 그리디 - 동전 0 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/11047
+import sys
+input = sys.stdin.readline
+
+n, k = list(map(int, input().split()))
+
+coins = [0] * n
+for i in range(n):
+  coins[i] = int(input())
+
+coins.reverse()
+
+total_coins = 0
+for coin in coins:
+  if(k > coin):
+    total_coins += k // coin
+    k = k % coin
+
+print(total_coins)
