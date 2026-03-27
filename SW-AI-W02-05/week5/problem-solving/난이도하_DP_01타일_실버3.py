f5304d587f59c858a47c1b439ae48532ba023c5d
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-27 21:05:00 +0900

    feat: dp 01타일 풀이 구현 #115

--- SW-AI-W02-05/week5/problem-solving/난이도하_DP_01타일_실버3.py
+++ SW-AI-W02-05/week5/problem-solving/난이도하_DP_01타일_실버3.py
@@ -1,2 +1,15 @@
 # DP - 01타일 (백준 실버3)
 # 문제 링크: https://www.acmicpc.net/problem/1904
+import sys
+input = sys.stdin.readline
+
+num = int(input())
+
+memo = [0] * (num+1)
+memo[0] = 1
+memo[1] = 1
+
+for i in range(2, num+1):
+  memo[i] = (memo[i-1] + memo[i-2]) % 15746
+
+print(memo[num])
