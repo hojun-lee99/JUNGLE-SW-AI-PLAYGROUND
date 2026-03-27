5e0b29421c3f340b4f4a3e5ec6bd4609b17031df
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-27 21:47:27 +0900

    feat: greedy 잃어버린 괄호 풀이 구현 #117

--- SW-AI-W02-05/week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ SW-AI-W02-05/week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,19 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+split_minus = input().split('-')
+
+results = []
+
+for nums in split_minus:
+  if('+' in nums):
+    nums_list = list(map(int, nums.split('+')))
+    results.append(sum(nums_list))
+  else:
+    results.append(int(nums))
+
+result = results[0]
+
+for i in range(1,len(results)):
+  result -= results[i]
+
+print(result)
