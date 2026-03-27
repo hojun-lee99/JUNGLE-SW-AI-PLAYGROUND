2f7a5115484998b89d90142ee860c981366bee63
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-27 17:42:13 +0900

    feat: dp 피보나치 수 2 풀이 구현 #114

--- SW-AI-W02-05/week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
+++ SW-AI-W02-05/week5/problem-solving/난이도하_DP_피보나치수2_브론즈1.py
@@ -1,2 +1,15 @@
 # DP - 피보나치 수 2 (백준 브론즈 1)
 # 문제 링크: https://www.acmicpc.net/problem/2748
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+memory = [0] * (n+1)
+
+memory[1] = 1
+
+for i in range(2, n+1):
+  memory[i] = memory[i-1] + memory[i-2]
+
+print(memory[n])
