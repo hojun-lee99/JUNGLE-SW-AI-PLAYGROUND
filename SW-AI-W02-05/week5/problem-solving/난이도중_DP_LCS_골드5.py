6ebdc4e085c3768e6f1a88f680f3e23da7614192
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-28 10:59:01 +0900

    refactor: dp LCS 입력 문자열 전처리 간소화 #119

--- SW-AI-W02-05/week5/problem-solving/난이도중_DP_LCS_골드5.py
+++ SW-AI-W02-05/week5/problem-solving/난이도중_DP_LCS_골드5.py
@@ -3,13 +3,9 @@
 import sys
 input = sys.stdin.readline
 
-string1 = list(input().strip())
-string2 = list(input().strip())
+list_string1 = "0" + input().strip()
+list_string2 = "0" + input().strip()
 
-list_string1 = [0]
-list_string2 = [0]
-list_string1.extend(string1)
-list_string2.extend(string2)
 
 x = len(list_string1)
 y = len(list_string2)
