17617a9a03d6ff804713ee615c15ae81ca4be580
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-31 19:32:46 +0900

    feat: dp 동전 풀이 구현 #118

--- SW-AI-W02-05/week5/problem-solving/난이도중_DP_동전_골드5.py
+++ SW-AI-W02-05/week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,20 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+  _n = int(input())
+  coins = list(map(int, input().split()))
+  total_amount = int(input())
+  
+  dp = [0] * (total_amount+1)
+  dp[0] = 1
+  
+  for coin in coins:
+    for i in range(coin, total_amount+1, 1):
+      dp[i] += dp[i-coin]
+
+  print(dp[total_amount])
