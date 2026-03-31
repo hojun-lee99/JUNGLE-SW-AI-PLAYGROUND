3e9ff57486477384167a4ee035377053cdec2355
hojun-lee99 <55483831+hojun-lee99@users.noreply.github.com>
2026-03-31 14:45:48 +0900

    feat: dp 평범한 배낭 풀이 구현 #120

--- SW-AI-W02-05/week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
+++ SW-AI-W02-05/week5/problem-solving/난이도중_DP_평범한배낭_골드5.py
@@ -1,2 +1,25 @@
 # DP - 평범한 배낭 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/12865
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+stuffs = [0] * (n+1)
+matrix = [[0 for _ in range(k+1)] for _ in range(n+1)]
+
+for i in range(1, n+1):
+  w, v = map(int, input().split())
+  stuffs[i] = (w, v)
+
+for i in range(1, n+1):
+  for j in range(1, k+1):
+    prev_row = i-1
+    stuff_weight = stuffs[i][0]
+    stuff_value = stuffs[i][1]
+    if j < stuff_weight:
+      matrix[i][j] = matrix[prev_row][j]
+    else:
+      matrix[i][j] = max(matrix[prev_row][j], matrix[prev_row][j-stuff_weight] + stuff_value)
+
+print(matrix[n][k])
